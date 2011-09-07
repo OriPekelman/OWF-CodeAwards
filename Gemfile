2bbaecd56d88053589aeb6d6b60fe69c9c59f5db
Ori Pekelman <ori@af83.com>
2011-09-07 14:49:20 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+gem 'rails', '3.1.0'
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+gem "rspec-rails", ">= 2.6.1", :group => [:development, :test]
+gem "database_cleaner", ">= 0.6.7", :group => :test
+gem "mongoid-rspec", ">= 1.4.4", :group => :test
+gem "factory_girl_rails", ">= 1.2.0", :group => :test
+gem "devise", ">= 1.4.4"
+gem "bson_ext", ">= 1.3.1"
+gem "mongoid", ">= 2.2.0"
+gem "omniauth", ">= 0.2.6"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeAwards::Application.config.secret_token = 'e2f968c6de51a590322d844d1356a4c078cbbcda9409a39c0afa9918eb925b3c98793f246e8612c2b6438276301ce932884dcae6034544f48b6579f8dd6f4369'
